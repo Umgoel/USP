fe13925c730710d6bc63620c69d0acd912ce8c77
Umgoel <76695806+Umgoel@users.noreply.github.com>
2023-01-03 19:06:56 +0530

    Create P2.sh
    Shell script to count the number of VOWELS OF A STRING

---
+++ Week 7/P2.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Enter string : \c"
+read str
+counter=0
+i=1
+len=${#str}
+while [ $i -le $len ] ; do
+ch=$(echo $str | cut -c "$i")
+if [ $ch = "a" -o $ch = "e" -o $ch = "i" -o $ch = "o" -o $ch = "u" ] ; then
+counter=$(($counter+1))
+fi
+i=$(($i+1))
+done
+echo "Number of vowels in $str is $counter"
